86475610e3a04ef2e769a3cdb8b99a4799e17075
tope96 <39431066+tope96@users.noreply.github.com>
2018-11-03 22:52:52 +0100

    Update cS.js

--- TIN/IV/C/cS.js
+++ TIN/IV/C/cS.js
@@ -3,7 +3,7 @@ function add(name, sName){
         alert("Podaj wartości!");
     }else{
         var table = document.getElementById("myTable");
-        var row = table.insertRow(-1);
+        var row = table.insertRow(-1); //-1 for adding rows at the end of the table
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
         cell1.innerHTML = name;
